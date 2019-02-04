3ba686543b5870333a8062870b47b7f328ce518d
Massimiliano <massimiliano.brocchini@gmail.com>
2019-02-04 21:21:28 +0100

    prevents firefox from starting maximized

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -515,9 +515,9 @@ awful.rules.rules = {
       }, properties = { titlebars_enabled = true }
     },
 
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    -- Firefox should not be maximized
+    { rule = { class = "Firefox" },
+       properties = {maximized = false } },
 }
 -- }}}
 
