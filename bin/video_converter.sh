89c430310b2966a78f70ddd66beddca10351b02d
Massimiliano Brocchini <massimiliano.brocchini@gmail.com>
2026-02-08 22:16:35 +0100

    rename original file after video conversion (makes more sense if ffmpeg is interrupted)

--- bin/video_converter.sh
+++ bin/video_converter.sh
@@ -7,7 +7,7 @@ m=$(mediainfo --Output="Video;%CodecID%" "$src")
 if [[  "$m" == "MJPG" || "$m" == "avc1" ]]; then
 	d=$(stat -c %y $src)
 
-	mv "$src" "$src.orig"
-	ffmpeg -threads "$(cat /proc/cpuinfo | grep "^processor" | wc -l)" -i "$src.orig" -vcodec libx264 "$dst"
+	ffmpeg -threads "$(cat /proc/cpuinfo | grep "^processor" | wc -l)" -i "$src" -vcodec libx264 "$dst"
 	touch --date="$d" "$dst"
+	mv "$src" "$src.orig"
 fi
