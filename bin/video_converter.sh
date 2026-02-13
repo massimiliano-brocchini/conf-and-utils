08335c876cca6d3622c49ebbe216e7dc224cc7a1
Massimiliano Brocchini <massimiliano.brocchini@gmail.com>
2026-02-13 08:36:34 +0100

    Intel QuickSync hardware acceleration

--- bin/video_converter.sh
+++ bin/video_converter.sh
@@ -7,7 +7,7 @@ m=$(mediainfo --Output="Video;%CodecID%" "$src")
 if [[  "$m" == "MJPG" || "$m" == "avc1" ]]; then
 	d=$(stat -c %y $src)
 
-	ffmpeg -xerror -threads "$(cat /proc/cpuinfo | grep "^processor" | wc -l)" -i "$src" -vcodec libx264 "$dst" || exit 1
+	ffmpeg -xerror -threads "$(cat /proc/cpuinfo | grep "^processor" | wc -l)" -i "$src" -vcodec h264_qsv "$dst" || exit 1
 	touch --date="$d" "$dst"
 	mv "$src" "$src.orig"
 fi
